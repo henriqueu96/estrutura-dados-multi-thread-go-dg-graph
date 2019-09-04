508766408ab27f33de935db0206897f3ba717d1e
Kim Schaurich <kimschaurich@gmail.com>
2019-09-03 21:11:07 -0300

    adicionado os case

--- src/dgGraph/dgNode.go
+++ src/dgGraph/dgNode.go
@@ -1,5 +1,7 @@
 package dgGraph
 
+import "errors"
+
 type dgNode struct {
 	status                   dgNodeStatus
 	request                  *request
@@ -11,14 +13,54 @@ type dgNode struct {
 	NextNodeInManagementChannel chan ManagementMessage
 }
 
-func (node *dgNode) start() {
+func (node *dgNode) start() error {
 	for {
 		message := <-node.inManagementChannel
 		switch message.messageType {
+		case enterNewNode:
+			// todo: fazer a mensagem de novo node chegando
 		case newNodeAppeared:
-			// some code (async to maintain node receiving messages)....
-		case incrementConflict:
-			// some code (async to maintain node receiving messages)....
+			if node.status == entering {
+				if node.NextNodeInManagementChannel == nil {
+					node.status = ready
+					go node.start()
+				} else {
+					node.NextNodeInManagementChannel <- NewManagementMessage(newNodeAppeared, node)
+					// todo: quando receber o nodo, pegar o request e ver se conflita, após, atribuir o next chanel para o  node.inManagementChannel
+				}
+
+			} else {
+				return errors.New("") // todo: definir texto do erro
+			}
+		case hasConflictMessage:
+			if node.status != entering {
+				return errors.New("") // todo: definir texto do erro
+			} else {
+				node.dependenciesNumber++
+			}
+		case endsConflictMessage:
+			if node.status != entering {
+				return errors.New("") // todo: definir texto do erro
+			}
+			node.status = waiting
+
+			if node.dependenciesNumber == node.solvedDependenciesNumber {
+				node.status = ready
+				go node.start()
+			}
+		case decreaseConflict:
+			node.solvedDependenciesNumber++ // mais uma dep resolvida
+			if node.status == waiting && node.dependenciesNumber == node.solvedDependenciesNumber { // era a ultima ?
+				node.status = ready
+				go node.start()
+			}
+		case endFunc:
+			node.status = leaving // marca como acabou de executar, saindo */
+			for _, e := range node.dependentsChannelsList {
+				e <- decreaseConflict
+			}
+
+			//mchi <- freeMSG -- todo: perguntar dotti
 		}
 	}
 }
--- src/dgGraph/managementMessage.go
+++ src/dgGraph/managementMessage.go
@@ -2,21 +2,26 @@ package dgGraph
 
 type ManagementMessage struct {
 	messageType ManagementMessageType
-	parameter interface{}
+	parameter   interface{}
 }
 
-func NewManagementMessage(messageType ManagementMessageType, parameter interface{}) ManagementMessage{
+func NewManagementMessage(messageType ManagementMessageType, parameter interface{}) ManagementMessage {
 	return ManagementMessage{
 		messageType: messageType,
-		parameter:  parameter,
+		parameter:   parameter,
 	}
 }
-
 
 type ManagementMessageType int;
 
 const (
-	newNodeAppeared   ManagementMessageType = 0
-	incrementConflict ManagementMessageType = 1
-	decreaseConflict  ManagementMessageType = 2
+	enterNewNode        ManagementMessageType = 0 //liberando nodo para entrar no grapho
+	newNodeAppeared     ManagementMessageType = 1 // nodo pergunta se tem conflitos
+	hasConflictMessage  ManagementMessageType = 2 // tem conflito
+	endsConflictMessage ManagementMessageType = 3 // nodo mais antigo avisa que terminou as mensagens de conflito
+	decreaseConflict    ManagementMessageType = 4 // decrementa um conflito
+	endFunc             ManagementMessageType = 5 // termina execução
+
+
+
 )
