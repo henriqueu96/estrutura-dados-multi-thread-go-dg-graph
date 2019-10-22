15d01c8a3ac2fef6362eb9c4a5168dd5b2173fb0
Henrique Urruth <henriqueu96@gmail.com>
2019-10-22 19:24:44 -0300

    updating scripts

---
+++ src/run-for-test.sh
@@ -0,0 +1,4 @@
+sh ./run.sh 8 200 0 200 3 ./results/result-with-no-conflicts.txt
+sh ./run.sh 8 200 0.1 200 3 ./results/result-with-10percent-conflicts.txt
+sh ./run.sh 8 200 0.2 200 3 ./results/result-with-20percent-conflicts.txt
+sh ./run.sh 8 200 1 200 3 ./results/result-with-all-conflicts.txt
